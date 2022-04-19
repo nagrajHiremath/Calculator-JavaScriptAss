9f7a323fb42527eafe1d22b3bb1e36d69c811a80
Nagraj <91442450+nagrajHiremath@users.noreply.github.com>
2022-04-19 18:11:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var displayArea = document.getElementById('display')
+
+function isOperator(buttonText){
+    if(buttonText == '+' || buttonText == '-' || buttonText == '/' || buttonText == '*' || buttonText == '%')
+    return true
+    else
+    return false
+}
+
+// function buttonClicked(buttonText){
+//     if(displayArea.innerText.length >= 25){
+//         return
+//     }
+//     if(buttonText != 'CLR' && buttonText != 'DEL' && buttonText != '='){
+//     if(displayArea.innerText === '0' ){
+//         if(buttonText != '00' && displayArea.innerText != '00'){
+//             if(buttonText == '.' || isOperator(buttonText)){
+//                 displayArea.innerText = displayArea.innerText + buttonText
+//             }
+//             else{
+             
+//                 displayArea.innerText = buttonText
+//             }
+//         }
+        
+//     }
+//     else{
+        
+//         if(isOperator(displayArea.innerText[displayArea.innerText.length-1]) && isOperator(buttonText)  ){
+//             //replace
+//             displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText
+//         }
+//         else{
+//             displayArea.innerText = displayArea.innerText + buttonText
+//         }
+//     }
+// }
+// else{
+//     if(buttonText === 'CLR'){
+//         displayArea.innerText = 0
+//     }
+//     else if(buttonText === 'DEL'){
+//         displayArea.innerText = displayArea.innerText.slice(0,-1)
+//     }
+//     else {
+//         try{
+//             displayArea.innerText =  eval(displayArea.innerText)
+//         }
+//         catch(error){
+//             displayArea.innerText = 0
+//         }
+        
+//     }
+// }
+// } 
+
+
+function buttonClicked(buttonText){
+    if(displayArea.innerText.length >= 25){
+        return
+    }
+    if(buttonText != 'CLR' && buttonText != 'DEL' && buttonText != '='){
+    if(displayArea.innerText === '0' ){
+        if(buttonText != '00' && displayArea.innerText != '00'){
+            if(buttonText == '.' || isOperator(buttonText)){
+                displayArea.innerText = displayArea.innerText + buttonText
+            }
+            else{
+             
+                displayArea.innerText = buttonText
+            }
+        }
+        
+    }
+    else{
+        
+        if(isOperator(displayArea.innerText[displayArea.innerText.length-1]) && isOperator(buttonText)  ){
+            //replace
+            displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText
+        }
+        else{
+            displayArea.innerText = displayArea.innerText + buttonText
+        }
+    }
+}
+else{
+    if(buttonText === 'CLR'){
+        displayArea.innerText = 0
+    }
+    else if(buttonText === 'DEL'){
+        displayArea.innerText = displayArea.innerText.slice(0,-1)
+    }
+    else {
+        try{
+            displayArea.innerText =  eval(displayArea.innerText)
+        }
+        catch(error){
+            displayArea.innerText = 0
+        }
+        
+    }
+}
+} 
